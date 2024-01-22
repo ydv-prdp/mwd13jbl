67cd5c2aec915f0b40ac710ab32117147e655ea7
Pradeep Yadav <pradeepyadav101010@gmail.com>
2024-01-22 21:26:34 +0530

    scroll up button added

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,7 +47,13 @@ let swiper = new Swiper('.favorite__swiper',{
 })
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY>=350? scrollUp.classList.add('show-scroll')
+                    : scrollUp.classList.remove('show-scroll')
+}
 
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
