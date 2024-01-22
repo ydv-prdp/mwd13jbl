ca438fc7a38d265cf7254abef40ad3eea2b4b775
Pradeep Yadav <pradeepyadav101010@gmail.com>
2024-01-22 16:56:57 +0530

    Added about section and blurheader

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,13 @@ navLink.forEach(n=>n.addEventListener('click',linkAction))
 
 
 /*=============== ADD BLUR HEADER ===============*/
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY>=50 ? header.classList.add('blur-header')
+                    : header.classList.remove('blur-header')
+}
 
+window.addEventListener('scroll',blurHeader)
 
 /*=============== SWIPER FAVORITES ===============*/ 
 
